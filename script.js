9ad2652c9173fe5fc76c6e4766ad3c71fca11c6a
Emiliano Cattaneo <emiliano_cattaneo@hotmail.com>
2023-02-15 19:41:39 -0300

    base de datos de seguros

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 {
-    function calcularEdad(valorA) {
-        return 2023 - valorA;
-    }
-
-    function mayorEdad(valorE) {
-        if (valorE <= 17) {
-            alert(`Hola ${nombreUsuario} ${apellidoUsuario} supongo que tenes ${valorE} años. Eres menor de edad`);
+    /* ARRAY DE LA BASE DE DATOS */
+    class Lista{
+        constructor(marca, precio){
+            this.marca = marca;
+            this.precio = precio;
         }
-        else
-            alert(`Hola ${nombreUsuario} ${apellidoUsuario} supongo que tenes ${valorE} años. Eres mayor de edad`);
     }
+    
+    /* BASE DE DATOS DE MARCAS Y TARIFAS */
+    const volkswagen = new Lista ("Volkswagen", 3100);
+    const ford = new Lista ("Ford", 2840);
+    const renault = new Lista ("Renault", 3220);
+    const chevrolet = new Lista ("Chevrolet", 2750);
+    const peugeot = new Lista ("Peugeot", 3845);
+    const fiat = new Lista ("Fiat", 2530);
 
+    const listaProductos = [volkswagen, ford, renault, chevrolet, peugeot, fiat];
+    
+    /* PROCESO PARA CONFIRMAR LAS CONTRASEÑAS */
     function confirmacion(valorC, valorU) {
         for (let contador = 2; contador >= 0; contador--) {
             valorC = prompt("Ingrese nuevamente su contraseña:");
             parseInt = (valorC);
 
             if (valorC == valorU) {
-                alert("Bienvendio");
+                alert("Puede seguir con el proceso.");
                 console.log("Confirmacion = Si");
                 break;
             }
@@ -34,23 +41,27 @@
         }
     }
 
-    let nombreUsuario = prompt("Cual es tu nombre? (sin apellido)");
-    let apellidoUsuario = prompt("Cual es tu apellido?");
-    console.log(`Nombre y Apellido: ${nombreUsuario} ${apellidoUsuario}`);
+    /* SE PIDE LOS DATOS DEL USUARIO */
+    let nombreUsuario = prompt("Cual es su nombre completo?");
+    let edadUsuario = prompt("Cual es su edad?");
+    console.log(`Nombre completo y edad: ${nombreUsuario}, ${edadUsuario}`);
+    
+    /* SE PIDE LOS DATOS DEL AUTO DEL USUARIO*/
+    let marcaUsuario = prompt("Escriba la marca: \nVolkswagen // Ford // Renault // Chevrolet // Peugeot // Fiat");
+    let anioUsuario = prompt("Ingrese año del auto:");
+    console.log(`Marca:${marcaUsuario}. Año:${anioUsuario}.`);
+    const buscar = listaProductos.find (Lista=> Lista.marca === marcaUsuario);
+    console.log(buscar);
 
-    let anioNacimiento = prompt("En que año naciste?");
-    parseInt(anioNacimiento);
-    console.log(`Nacimiento: ${anioNacimiento}`);
-
-    let edad = calcularEdad(anioNacimiento)
-    console.log(`Edad aproximada: ${edad}`);
-
-    mayorEdad(edad);
-
-    let passwordUsuario = prompt("Ingrese su contraseña:");
+    /* SE CALCULA LA TARIFA TOTAL CON LOS DATOS */
+    prompt(`El costo de seguro por su ${marcaUsuario} año ${anioUsuario} es de `)
+    
+    /* EN CASO DE CONFIRMAR EL SEGURO SE LE PIDE UNA CONTRASEÑA */        
+/*     let passwordUsuario = prompt("Ingrese una contraseña:");
     parseInt = (passwordUsuario);
-    console.log(`Password: ${passwordUsuario}`);
+    console.log(`Password: ${passwordUsuario}`); */
 
-    let passwordConfirmacion;
-    confirmacion(passwordConfirmacion, passwordUsuario);
+    /* SE PIDE LA CONFIRMACION DE LA CONTRASEÑA */
+/*     let passwordConfirmacion;
+    confirmacion(passwordConfirmacion, passwordUsuario); */
 }
